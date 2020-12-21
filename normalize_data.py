824adfdd9a82c1e8b4227f03fc97bb059c681faa
Vlad <2beats4hits@gmail.com>
2020-12-21 10:47:33 +0200

    Alternative translate version using aiohttp

--- normalize_data.py
+++ normalize_data.py
@@ -16,16 +16,16 @@ def split_to_sentences(text):
 
 if __name__ == '__main__':
     filenames = os.listdir(INPUT_DIR)
-    with open('dataset/splitted_by_regexp.txt', 'a') as fout:
+    with open('dataset/splitted_by_regexp.txt', 'w') as fout:
         for filename in filenames:
             with open(f'{INPUT_DIR}/{filename}', 'r') as file:
                 # with open('dataset/unnorm.txt', 'r') as file:
-                filetext = file.read()
+                filetext = re.sub(' +', ' ', file.read())
 
-            filetext = filetext.replace('\n', ' ')
-            filetext = filetext.replace('\t', ' ')
+            filetext = filetext.replace('\n', '')
+            filetext = filetext.replace('\t', '')
 
-            filetext = '\n'.join([sent for sent in split_to_sentences(filetext)])
+            filetext = '\n'.join([sent.strip() for sent in split_to_sentences(filetext)])
             print(filetext)
 
             fout.write(filetext)
--- translate.py
+++ translate.py
@@ -1,78 +1,37 @@
 #!/usr/bin/env python3
-import re
+import asyncio
 
-from requests import post
+import aiofiles
+import aiohttp
 
 MAX_SIZE = 4800
 
-def translate(entry):
+
+async def translate(entry):
     body = {
         'text': entry,
         'gfrom': 'ru',
         'gto': 'uz',
         'key': '2956684590ru85'
     }
-    result = post('https://www.webtran.ru/gtranslate/', data=body)
-    return result.text
+    async with aiohttp.ClientSession() as session:
+        async with session.post('https://www.webtran.ru/gtranslate/',
+                                data=body) as response:
+            return await response.text()
 
 
-def split_to_sentence_chunks(sentences, max_size):
-    result = []
-    chunk = []
-    for sentence in sentences:
-        if sum([len(sent) for sent in chunk]) + len(sentence) < max_size:
-            chunk.append(sentence)
-        elif len(sentence) > max_size:
-            print('Long sentence found')
-            pass
-        else:
-            result.append(''.join(chunk))
-            chunk = []
-    if chunk != []:
-        result.append(''.join(chunk))
-    return result
+async def main():
+    with open('dataset/splitted_by_regexp.txt', 'r') as fin:
+        sentences = fin.readlines()
+    async with aiofiles.open('dataset/dataset_by_regexp_result.txt', 'w') as fout:
+        for line in sentences:
+            translation = await translate(line)
+            print(f'[Done] chunk {translation}')
+            final_sentence = f"{line}\t{translation}"
+            await fout.write(final_sentence)
+            await fout.flush()
 
-
-# def splitkeepsep(s, sep): return reduce(lambda acc, elem: acc[:-1] + [acc[-1] + elem] if elem == sep else acc + [elem], re.split("(%s)" % re.escape(sep), s), [])
-
-
-# if __name__ == '__main__':
-#     filenames = [entry for entry in listdir(INPUT_DIR) if isfile('/'.join((abspath(INPUT_DIR), entry)))]
-#     for filename in filenames:
-#         src_path = '/'.join((abspath(INPUT_DIR), filename))
-#         dest_path = '/'.join((abspath(OUTPUT_DIR), filename))
-#         translation = ''
-#         filetext = ''
-
-#         print('[INFO]: Processing file ' + filename + '. . .')
-#         print('[INFO]: in : ' + src_path)
-#         print('[INFO]: out: ' + dest_path)
-#         with open(src_path, 'r') as fin:
-#             filetext = fin.read().replace('\n', ' ')
-
-#         for chunk in split_to_sentence_chunks(filetext, MAX_SIZE):
-#             translation += ' '
-#             translation += translate(chunk).replace('\n', ' ')
-
-#         with open(dest_path, 'w') as fout:
-#             fout.write(translation)
-#         print('[INFO]: Done with ' + filename)
 
 if __name__ == "__main__":
-    sentences = []
-    with open('dataset/norm.txt', 'r') as fin:
-        sentences = fin.readlines()
-
-    with open('dataset/dataset1.txt', 'w') as fout:
-        for i, chunk in enumerate(split_to_sentence_chunks(sentences, MAX_SIZE)):
-            translation = translate(chunk)
-            sents1 = chunk.split('\n')
-            sents2 = translation.split('\n')
-            if len(sents1) == len(sents2):
-                print('eq')
-                fout.writelines(['\t'.join(s) for s in zip((sents1, sents2))])
-            else:
-                print(f'not eq: {len(sents1)} - {len(sents2)}')
-                # print(f'[] {chunk} - {translation}')
-            # fout.write()
-            print(f'[Done] chunk {i}')
+    loop = asyncio.get_event_loop()
+    loop.run_until_complete(main())
